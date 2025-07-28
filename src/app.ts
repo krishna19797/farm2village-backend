b97491c946a3a778c892555c83c031fef28d7df0
krishna19797 <krishnah120@gmail.com>
2025-07-27 21:29:03 -0500

    Fix cors issue

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 import userRoutes from './routes/userRoutes';
 
 const app = express();
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+}));
 
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
 
 app.use('/api/users', userRoutes);
 
